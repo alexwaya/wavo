0a7fec6ad1ba7e354c6754e34ad40f7cef9dec00
LynnOdero <oderolynn@gmail.com>
2018-05-17 15:43:57 +0300

    styling on homepage

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+$("#button2").click(function() {
+	alert("successfully subscribed");
+     // var emailAddress = $("#Email").val();
+     // alert(.emailAddress +" has been successfully added to our email list.Thank you. **")
+});
+});
+
+$(document).ready(function () {
+$("#button3").click(function() {
+	alert("successfully subscribed");
+     // var emailAddress = $("#Email").val();
+     // alert(.emailAddress +" has been successfully added to our email list.Thank you. **")
+});
+});
+
+$(document).ready(function () {
+$("#button4").click(function() {
+	alert("successfully subscribed");
+     // var emailAddress = $("#Email").val();
+     // alert(.emailAddress +" has been successfully added to our email list.Thank you. **")
+});
+});
