b0695374d7f6fa123a6a67e9417445526dc0d0f4
vonmutinda <maxwellwambua@gmail.com>
2018-05-18 09:38:07 +0300

    fonts worked out

--- assets/js/von-script.js
+++ assets/js/von-script.js
@@ -1,27 +1,10 @@
 $(document).ready(function() {
     
-function Account(username,pass,email){
-    this.username = username;
-    this.password = pass;
-    this.email = email;
-}
-
-
-
-
-
-
-
-
-
-
     $('#menu').click(function() {
         $('#hiddenAgenda').show();
-        $('.feed').addClass('dim');
     });
 
       $('#unMenu').click(function () {
           $('#hiddenAgenda').hide().slideUp(500);
-          $('.feed').removeClass('dim');
       });
 });
